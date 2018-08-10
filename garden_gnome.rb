82a0589f54e8563dc4c18383e00e23ae471cccff
maevet78 <190439mt@saintviator.com>
2018-08-10 17:01:14 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,29 +1,31 @@
 # Code your instances here
 class GardenGnome
+  attr_accessor (:name, :age, :gluten_allergy)
+  attr_
    def initialize (personality = "evil")
     @personality = personality
   end   
   
-  def name=(name)
-    @name = name
-  end 
-  def name
-    @name 
-  end
+  # def name=(name)
+  #   @name = name
+  # end 
+  # def name
+  #   @name 
+  # end
   
-  def age=(age)
-    @age = age
-  end
-  def age
-    @age
-  end  
+  # def age=(age)
+  #   @age = age
+  # end
+  # def age
+  #   @age
+  # end  
   
-  def gluten_allergy=(gluten_allergy)
-    @gluten_allergy = gluten_allergy
-  end 
-  def gluten_allergy
-    @gluten_allergy
-  end  
+  # def gluten_allergy=(gluten_allergy)
+  #   @gluten_allergy = gluten_allergy
+  # end 
+  # def gluten_allergy
+  #   @gluten_allergy
+  # end  
   
 
 end  
